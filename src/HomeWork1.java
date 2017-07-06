1cad6d25f747d8874584f606cbe2774f31a293b3
AndYar <goldensever@gmail.com>
2017-07-06 17:48:03 +0300

    HomeWork#1

---
+++ src/HomeWork1.java
@@ -0,0 +1,9 @@
+public class HomeWork1{
+    public static void main (String[] args){
+        System.out.println("Как я проведу лето 2017?");
+        System.out.println("Буду все лето обучаться!:)");
+    }
+}
+
+
+
